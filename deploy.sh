b60f0d921de2f35d8b4e98ded84321181f005ca6
Bernardo Hummes <hummesbe@gmail.com>
2025-07-01 17:49:15 +0200

    Deploy: Tue Jul  1 17:49:15 CEST 2025 (80f755d)

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,17 @@ set -e
 echo "Building site..."
 forester build
 
-echo "Deploying to deploy branch..."
-git checkout deploy
-git rm -rf .
-git checkout master -- .
-cp -r output/* .
-rm -rf output/ build/ assets/ deploy.sh flake.nix forest.toml theme/ trees/
+# echo "Deploying to deploy branch..."
+# git checkout deploy
+# git rm -rf .
+# git checkout master -- .
+# cp -r output/* .
+# rm -rf output/ build/ assets/ deploy.sh flake.nix forest.toml theme/ trees/
 
 git add .
 git commit -m "Deploy: $(date) ($(git rev-parse --short main))"
-git push origin deploy
+# git push origin deploy
+git push origin master
 
-git checkout master
+# git checkout master
 echo "Deployed successfully!"
