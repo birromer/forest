a16d76d8b7a29548e7d8cf1a5b51554ef7d5dc40
Bernardo Hummes <hummesbe@gmail.com>
2025-07-01 16:37:23 +0200

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ forester build
 echo "Deploying to deploy branch..."
 git checkout deploy
 git rm -rf .
-git checkout main -- output/
+git checkout master -- .
 cp -r output/* .
-rm -rf output/
+rm -rf output/ build/ assets/ deploy.sh flake.nix forest.toml theme/ trees/
 
 git add .
 git commit -m "Deploy: $(date) ($(git rev-parse --short main))"
 git push origin deploy
 
-git checkout main
+git checkout master
 echo "Deployed successfully!"
