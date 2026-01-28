e864e683a8f957ae936f6624f6aac1be911857fc
Bernardo Hummes <hummesbe@gmail.com>
2026-01-28 07:43:36 +0100

    started notes struct

---
+++ trees/.zettel.lua
@@ -0,0 +1,11 @@
+return {
+  dir = "trees/notes",
+  sep = "-",
+  ext = ".tree",
+  index = nil,
+  notions = nil,
+  notations = "macros.tree",
+  scratchpad = "scratch.tree",
+  id_max = 46655,  -- 4 base36 digits: ZZZZ
+  id_size = 4
+}
